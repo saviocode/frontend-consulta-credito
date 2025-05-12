2660372313ae6309f24ff4831381d5db62bc158f
savio nascimento <savio.nascimento.gomes@gmail.com>
2025-05-12 09:04:20 -0300

    create SkeletonCardComponent

---
+++ src/app/shared/skeleton/skeleton-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-skeleton-card',
+  templateUrl: './skeleton-card.component.html',
+  styleUrls: ['./skeleton-card.component.scss'],
+  standalone: true,
+  imports: []
+})
+export class SkeletonCardComponent { }
